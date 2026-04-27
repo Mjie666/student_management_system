5556f7ed02c3ffb88b864647c8f8c0fd0307a291
简乐涵 <3428597298@qq.com>
2026-04-28 00:22:31 +0800

    2.0远程服务器

--- src/api/student.js
+++ src/api/student.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-const IPv4 = '192.168.1.146'
-const BASE = `http://${IPv4}:8080/api/student`
-
+/* const IPv4 = '192.168.1.146' */
+const BASE = 'https://hdstudentmanagement-production.up.railway.app/api/student'
 export const getStudent = () => axios.get(BASE)
 export const addStudent = (data) => axios.post(BASE, data)
 export const delStudent = (id) => axios.delete(`${BASE}/${id}`)
